671d5ad7ce9fb0b9d3a210992097489633ed36ff
Heron Silveira Lima <heronsilveiralima2@gmail.com>
2022-03-19 14:01:11 -0300

    first commmit

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -4,8 +4,9 @@ import br.com.dio.model.Gato;
 
 public class primeiroprograma {
 
+
     public static void main(String[] args){
-        Gato gato = new Gato { };
+        Gato gato = new Gato();
 
         System.out.println(Gato);
 
